977b79f22041c1cc5e9fbe9e1c490ea8d20dddc6
Omar Belfeki <164212057+OmarBelfeki@users.noreply.github.com>
2024-12-07 15:56:18 +0100

    Create schema.py

---
+++ schema.py
@@ -0,0 +1,17 @@
+from datetime import date
+from enum import Enum
+from pydantic import BaseModel, EmailStr
+
+
+class UserSchema(BaseModel):
+    email: EmailStr
+    username: str
+    password: str
+
+    class Config:
+        orm_mode = True
+
+
+class Roles(Enum):
+    user = "user"
+    admin = "admin"
